1cbc02aae9a0edb8a991986331ddfa69dc5f9f1b
I Hyeon Hong <ihyeonhong@gmail.com>
2024-10-10 12:49:49 +0900

    Add Day_2024_10_10

--- Day_2024_10_10/src/IfElseIfTest.java
+++ Day_2024_10_10/src/IfElseIfTest.java
@@ -1,18 +1,23 @@
 import java.util.Scanner;
 
-public class IfElseTest {
+public class IfElseIfTest {
 
 	public static void main(String[] args) {
-		// 1을 입력했을 때 메세지 표시,
+		// 1을 입력했을 때 메세지 표시
+		// 2를 입력했을 때 메세지 표시
 		// 아닐 경우 그 외 정수를 입력했을 때 메세지 표시
 		int num;
 		Scanner sc = new Scanner(System.in);
 		System.out.println("숫자를 입력해주세요.");
 		num = sc.nextInt();
 		if (num == 1) {
-			System.out.println("정수 1이 입니다.");
+			System.out.println("정수 1 입니다.");
+		} else if (num == 2) {
+			System.out.println("정수 2 입니다.");
+		} else if (num == 3) {
+			System.out.println("정수 3 입니다.");
 		} else {
-			System.out.println("정수 1이 아닙니다.");
+			System.out.println("1,2,3 중 입력 해주세요.");
 		}
 		System.out.println("프로그램 종료");
 		return;
--- Day_2024_10_10/src/IfTest.java
+++ Day_2024_10_10/src/IfTest.java
@@ -3,13 +3,13 @@ public class IfTest {
 
 	public static void main(String[] args) {
 		// 조건문 : true 또는 false로 값을 반환하는 식
-		// 부등호(>, <, >=, <=, ==, !=)
+		// 관계 연산자 혹은 비교 연산자(>, <, >=, <=, ==, !=)
 		int num = 10;
-		
+
 		System.out.println(num > 5);
 		System.out.println(num >= 10);
 		System.out.println(num == 5);
 		System.out.println(num != 5);
-		
+
 	}
 }
